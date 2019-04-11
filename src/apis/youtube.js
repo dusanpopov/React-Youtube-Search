e572f30e0ffd3b58be53fe2038ab65b626c97862
Dušan Popov <aristocut90@gmail.com>
2019-04-11 02:14:17 +0200

    formatting

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,17 +1,14 @@
 import axios from "axios";
 
-
 const API_KEY = "AIzaSyBPknu9EECIdzqvCJUqCU8nWf3DorJ7hZo";
 
 export default axios.create({
 
     baseURL: "https://www.googleapis.com/youtube/v3",
+    
     params: {
-
         part: "snippet",
         maxResults: 15,
         key: API_KEY
-
     }
-    
 });
